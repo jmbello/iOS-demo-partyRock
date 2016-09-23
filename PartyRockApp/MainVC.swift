faeb2554b35dc66a697e2c78649e6cf7389d6fa5
Jorge Manuel Bello <jbello@macbook-pro-de-posibillian.local>
2016-09-23 15:12:13 -0400

    adding controllers, fonts and views

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Jorge Manuel Bello on 23/9/16.
+//  Copyright © 2016 Posibillian. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+
+}
+
